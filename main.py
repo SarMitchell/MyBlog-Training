868b4cef86e581c31f1d913fc0b20c6fd18e91be
Sarmitchell <sara.mitchell96@hotmail.co.uk>
2026-03-16 15:05:14 +1000

    Added profile and made website more robust

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
 from typing import List
 #from flask_gravatar import Gravatar
+import os
 from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user, login_required
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
@@ -32,7 +33,7 @@ def gravatar_url(email, size=100, rating='g', default='retro', force_default=Fal
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 app.jinja_env.filters['gravatar'] = gravatar_url
@@ -54,7 +55,7 @@ login_manager.init_app(app)
 class Base(DeclarativeBase):
     pass
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI','sqlite:///posts.db')
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -256,13 +257,20 @@ def delete_post(post_id):
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    if current_user.is_authenticated:
+        return render_template("about.html", logged_in=True)
+    else:
+        return render_template("about.html", logged_in=False)
+
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    if current_user.is_authenticated:
+        return render_template("contact.html", logged_in=True)
+    else:
+        return render_template("contact.html", logged_in=False)
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+    app.run(debug=False)
